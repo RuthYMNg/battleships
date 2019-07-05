eddfe606c03e2c165ee2412bb2aee31d4bde1a6f
Ruth Ng <ruth.ym.ng@gmail.com>
2019-07-05 22:33:16 +0100

    Improve cell CSS and add images for splash and fire

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,57 +3,55 @@ import React from 'react';
 const Cell = props => {
   if (props.win && props.player === 'computer') {
     if (props.cell.name === "Carrier") {
-      return <p className='cell carrier'>CA</p>
+      return <div className='cell carrier'><p>CA</p></div>
     } else if (props.cell.name === "Battleship") {
-      return <p className='cell battleship'>B</p>
+      return <div className='cell battleship'><p>B</p></div>
     } else if (props.cell.name === "Cruiser") {
-      return <p className='cell cruiser'>C</p>
+      return <div className='cell cruiser'><p>C</p></div>
     } else if (props.cell.name === "Submarine") {
-      return <p className='cell submarine'>S</p>
+      return <div className='cell submarine'><p>S</p></div>
     } else if (props.cell.name === "Destroyer") {
-      return <p className='cell destroyer'>D</p>
+      return <div className='cell destroyer'><p>D</p></div>
     } else if (props.cell.isDiscovered) {
-      return <p className='cell blue'></p>
+      return <div className='cell blue'><img src='/splash.png'/></div>
     } else {
-      return <p className='cell sea'></p>
+      return <div className='cell sea'></div>
     }
   } else if (props.player === 'human') {
     if (props.cell.isShip && props.cell.isDiscovered) {
-      return <p className='cell red'>X</p>
+      return <div className='cell red'><img src='/fire.png'/></div>
     } else if (props.cell.name === "Carrier") {
-      return <p className='cell carrier'>CA</p>
+      return <div className='cell carrier'><p>CA</p></div>
     } else if (props.cell.name === "Battleship") {
-      return <p className='cell battleship'>B</p>
+      return <div className='cell battleship'><p>B</p></div>
     } else if (props.cell.name === "Cruiser") {
-      return <p className='cell cruiser'>C</p>
+      return <div className='cell cruiser'><p>C</p></div>
     } else if (props.cell.name === "Submarine") {
-      return <p className='cell submarine'>S</p>
+      return <div className='cell submarine'><p>S</p></div>
     } else if (props.cell.name === "Destroyer") {
-      return <p className='cell destroyer'>D</p>
+      return <div className='cell destroyer'><p>D</p></div>
     } else if (props.cell.isDiscovered) {
-      return <p className='cell blue'></p>
-    } else if (props.cell.isShip && props.player === 'computer') {
-      return <p className='cell' onClick={props.fire.bind(null, props.row, props.col)}>O</p>
+      return <div className='cell blue'><img src='/splash.png'/></div>
     } else if (props.player === 'computer' && props.inDev) {
-      return <p className='cell' onClick={props.fire.bind(null, props.row, props.col)}>.</p>
+      return <div className='cell' onClick={props.fire.bind(null, props.row, props.col)}>.</div>
     } else if (props.cell.isShip && props.inDev) {
-      return <p className='cell sea'>.</p>
+      return <div className='cell sea'>.</div>
     } else {
-      return <p className='cell sea'></p>
+      return <div className='cell sea'></div>
     }
   } else {
     if (props.cell.isShip && props.cell.isDiscovered) {
-      return <p className='cell red'>X</p>
+      return <div className='cell red'><img src='/fire.png'/></div>
     } else if (props.cell.isDiscovered) {
-      return <p className='cell blue'></p>
+      return <div className='cell blue'><img src='/splash.png'/></div>
     } else if (props.cell.isShip && props.player === 'computer' && props.inDev) {
-      return <p className={`cell ${props.turn === 'A' ? 'cell-hover' : ''} sea`} onClick={props.turn === 'A' ? props.fire.bind(null, props.row, props.col) : null}>.</p>
+      return <div className={`cell ${props.turn === 'A' ? 'cell-hover' : ''} sea`} onClick={props.turn === 'A' ? props.fire.bind(null, props.row, props.col) : null}>.</div>
     } else if (props.player === 'computer') {
-      return <p className={`cell ${props.turn === 'A' ? 'cell-hover' : ''} sea`} onClick={props.turn === 'A' ? props.fire.bind(null, props.row, props.col) : null}></p>
+      return <div className={`cell ${props.turn === 'A' ? 'cell-hover' : ''} sea`} onClick={props.turn === 'A' ? props.fire.bind(null, props.row, props.col) : null}></div>
     } else if (props.cell.isShip && props.inDev) {
-      return <p className={`cell ${props.turn === 'A' ? 'cell-hover' : ''}`}>.</p>
+      return <div className={`cell ${props.turn === 'A' ? 'cell-hover' : ''}`}>.</div>
     } else {
-      return <p className='cell sea'></p>
+      return <div className='cell sea'></div>
     }
   }
 };
